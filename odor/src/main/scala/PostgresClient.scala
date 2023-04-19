7969b34aac66ad71756810e68f666fccf2716f31
johannes karoff <johannes@karoff.net>
2023-04-19 11:09:24 +0200

    make connection initialization of PostgresClient lazy

--- odor/src/main/scala/PostgresClient.scala
+++ odor/src/main/scala/PostgresClient.scala
@@ -59,11 +59,9 @@ class PostgresConnectionPool(connectionString: String, maxConnections: Int)(impl
   def acquireConnection(): Future[PoolClient] = pool.connect().toFuture
 
   def useConnection[R](code: PostgresClient => Future[R]): Future[R] = async {
-    val poolClient = await(acquireConnection())
-    poolClient.on("error", (err: Any) => println(s"Postgres connection error: $err"))
-    val pgClient   = new PostgresClient(this, poolClient)
+    val pgClient   = new PostgresClient(this)
     val codeResult = await(code(pgClient).attempt)
-    poolClient.release()
+    pgClient.release()
     codeResult match {
       case Left(err)  => throw err
       case Right(res) => res
@@ -71,7 +69,25 @@ class PostgresConnectionPool(connectionString: String, maxConnections: Int)(impl
   }
 }
 
-class PostgresClient(val pool: PostgresConnectionPool, connection: PoolClient)(implicit ec: ExecutionContext) {
+class PostgresClient(val pool: PostgresConnectionPool)(implicit ec: ExecutionContext) {
+
+  private var pgClientIsInitialized = false
+  private var pgClientIsReleased    = false
+
+  private lazy val connection: Future[PoolClient] = async {
+    if (pgClientIsReleased) throw new IllegalStateException("PostgresClient already released")
+    else {
+      pgClientIsInitialized = true
+      val poolClient = await(pool.acquireConnection())
+      poolClient.on("error", (err: Any) => println(s"Postgres connection error: $err"))
+      poolClient
+    }
+  }
+
+  def release(): Unit = if (!pgClientIsReleased) {
+    pgClientIsReleased = true
+    if (pgClientIsInitialized) connection.foreach(_.release())
+  }
 
   val transactionSemaphore: Future[Semaphore[IO]] = Semaphore[IO](1).unsafeToFuture()
 
@@ -80,7 +96,7 @@ class PostgresClient(val pool: PostgresConnectionPool, connection: PoolClient)(i
     params: PARAMS = Void,
   ): Future[Unit] = async {
     await(
-      connection
+      await(connection)
         .query(
           command.sql,
           command.encoder.encode(params).map(_.orNull).toJSArray,
@@ -95,7 +111,7 @@ class PostgresClient(val pool: PostgresConnectionPool, connection: PoolClient)(i
     params: PARAMS = Void,
   ): Future[Vector[ROW]] = async {
     val result = await(
-      connection
+      await(connection)
         .query[js.Array[js.Any], js.Array[js.Any]](
           QueryArrayConfig[js.Array[js.Any]](query.sql),
           query.encoder.encode(params).map(_.orNull.asInstanceOf[js.Any]).toJSArray,
